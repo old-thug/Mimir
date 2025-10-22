bd6488469e2b2035f35cacd832a333188a1a78f5
old-thug <dryeggo93@gmail.com>
2025-10-21 22:26:10 -0700

    Initial Commit

---
+++ build/CMakeFiles/mimirc.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mimirc.dir/compiler/lib/file_system.c.obj"
+  "CMakeFiles/mimirc.dir/compiler/lib/file_system.c.obj.d"
+  "CMakeFiles/mimirc.dir/compiler/main.c.obj"
+  "CMakeFiles/mimirc.dir/compiler/main.c.obj.d"
+  "libmimirc.dll.a"
+  "mimirc.exe"
+  "mimirc.exe.manifest"
+  "mimirc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mimirc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
